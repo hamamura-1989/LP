48090d0415f44c910038d7537a4cbb1b1a5f5f89
hamamura <hamamura.ryohe@rexcross.com>
2022-04-18 18:43:34 +0900

    メール送信機能まで完了

--- php/confirm.php
+++ php/confirm.php
@@ -7,24 +7,43 @@
     <title>送信確認画面</title>
     <link rel="stylesheet" href="../css/stylesheet.css">
     <link rel="stylesheet" href="../css/sp_stylesheet.css">
-
 </head>
 
+<!-- 入力データを受け取る -->
+<?php
+$familyName = filter_input(INPUT_POST, 'familyName');
+$firstName = filter_input(INPUT_POST, 'firstName');
+$familyHiraganaName = filter_input(INPUT_POST, 'familyHiraganaName');
+$firstHiraganaName = filter_input(INPUT_POST, 'firstHiraganaName');
+$phoneNumber = filter_input(INPUT_POST, 'phoneNumber');
+$email = filter_input(INPUT_POST, 'email');
+$consultationContent = filter_input(INPUT_POST, 'consultationContent');
+//  ご相談内容は複数あるので配列で取得する
+$consultation = array();
+$consultation = filter_input(INPUT_POST, 'consultation',FILTER_DEFAULT, FILTER_REQUIRE_ARRAY);
+
+// 入力データのエスケープ処理関数を定義しておく(入力データにプログラムへの命令文があると悪意のある動作命令を受けてしまうため)
+function esc($input){
+    return htmlspecialchars($input,ENT_QUOTES, "UTF-8");
+}
+?>
+
+<!-- ここから表示画面本文 -->
 <body class="confirm">    
     <h2>送信確認画面</h2>
     <div class="confirm-message">
-        <p>以下の内容でお間違いないでしょうか。</p>
-        <p>よろしければ、「送信」を押してください</p>
+        <p>以下の内容でお間違いないでしょうか。</p> 
+        <p>よろしければ「送信」を押してください。</p>
     </div>
-    <form action="send.php" class="confirmForm">
+    <form action="send.php" class="confirmForm" method="post">
         <div class="confirmFormBox">
             <div class="confirmForm-item confirmForm-item1"><!-- お名前 -->
                 <p class="confirmForm-item-left confirmForm-item-left1">
                     お名前：
                 </p> 
                 <div class="confirmForm-item-right confirmForm-item-right1">
-                    <div class="confirmForm-item-right1-1">山田</div>
-                    <div class="confirmForm-item-right1-2">太郎</div>
+                    <div class="confirmForm-item-right1-1"><?php echo esc($familyName); ?></div>
+                    <div class="confirmForm-item-right1-2"><?php echo esc($firstName); ?></div>
                 </div>
             </div>
             <div class="confirmForm-item confirmForm-item2"><!-- ふりがな -->
@@ -32,39 +51,54 @@
                     ふりがな：
                 </p>
                 <div class="confirmForm-item-right confirmForm-item-right2">
-                    <div class="confirmForm-item-right2-1">やまだ</div>
-                    <div class="confirmForm-item-right2-2">たろう</div>
+                    <div class="confirmForm-item-right2-1"><?php echo esc($familyHiraganaName); ?></div>
+                    <div class="confirmForm-item-right2-2"><?php echo esc($firstHiraganaName); ?></div>
                 </div> 
             </div>
             <div class="confirmForm-item confirmForm-item3"><!-- 電話番号 -->
                 <p class="confirmForm-item-left confirmForm-item-left3">
                     電話番号：
                 </p> 
-                <div class="confirmForm-item-right confirmForm-item-right3">000-0000-0000</div>
+                <div class="confirmForm-item-right confirmForm-item-right3"><?php echo esc($phoneNumber); ?></div>
             </div>
             <div class="confirmForm-item confirmForm-item4"><!-- メールアドレス -->
                 <p class="confirmForm-item-left confirmForm-item-left4">
                     メールアドレス：
                 </p> 
-                <div class="confirmForm-item-right confirmForm-item-right4">oooooooooooo@gmail.com</div>
+                <div class="confirmForm-item-right confirmForm-item-right4"><?php echo esc($email); ?></div>
             </div>
             <div class="confirmForm-item confirmForm-item5"><!-- 相談内容 -->
                 <p class="confirmForm-item-left confirmForm-item-left5">
                     ご相談の内容：
                 </p> 
                 <div class="confirmForm-item-right confirmForm-item-right5">
-                    <div class="confirmForm-item-right5List">1ああああああああああああ</div>
-                    <div class="confirmForm-item-right5List">2いいいいいいいいいいい</div>
-                    <div class="confirmForm-item-right5List">3うううううううううううううう</div>
+                    <?php for ($i=0;$i<count($consultation);$i++) { ?>
+                        <div class="confirmForm-item-right5List">・<?php echo esc($consultation[$i]); ?></div>
+                    <?php } ?>
                 </div>
             </div>
             <div class="confirmForm-item confirmForm-item6"><!-- 本文 -->
                 <p class="confirmForm-item-left confirmForm-item-left6">
                     本文：
                 </p> 
-                <div class="confirmForm-item-right confirmForm-item-right6">Fjgせあっふぐ亜F不FhじゃdsjF非gせうrwfmんゔぁfhjfdさf。fskfskfヒアfjsh不hっファhfjdsh不rfjなfデュfhファhfjカウr奈kんりあkつsけんsgんかんしょうほうがかアイウエオかきくけコリアン寝具hfkvネモsっも亜kしgにsじるksこghさい</div>
+                <div class="confirmForm-item-right confirmForm-item-right6"><?php echo esc($consultationContent); ?></div>
             </div>
         </div>
+
+        <!-- パラメータを次の送信画面へ渡しておく -->
+        <input type="hidden" name="familyName" value="<?php echo $familyName; ?>">
+        <input type="hidden" name="firstName" value="<?php echo $firstName; ?>">
+        <input type="hidden" name="familyHiraganaName" value="<?php echo $familyHiraganaName; ?>">
+        <input type="hidden" name="firstHiraganaName" value="<?php echo $firstHiraganaName; ?>">
+        <input type="hidden" name="phoneNumber" value="<?php echo $phoneNumber; ?>">
+        <input type="hidden" name="email" value="<?php echo $email; ?>">
+        <?php for ($i=0; $i<count($consultation); $i++) { ?>
+            <input type="hidden" name="consultation[]" value="<?php echo $consultation[$i]; ?>">
+        <?php } ?>
+        <input type="hidden" name="consultationContent" value="<?php echo $consultationContent; ?>">
+        
+        <!-- 送信 -->
         <input type="submit" class="btn_send" value="送信する">
+
     </form>
 </body>
--- php/send.php
+++ php/send.php
@@ -1,24 +1,120 @@
 <!DOCTYPE html>
 <html lang="ja">
 <head>
-<?php
-// echo で出力する方法
-echo '<meta charset="UTF-8">';
-// 文字列リテラルとして書く方法（このままでは出力されません）
-$title = 'php で HTML を書く方法';
-// 文字列操作でつなげることも出来ます
-echo "<title>{$title}</title>";
-// 一時的に php タグから脱出する方法
-?>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>送信確認画面</title>
+    <link rel="stylesheet" href="../css/stylesheet.css">
+    <link rel="stylesheet" href="../css/sp_stylesheet.css">
 </head>
 
-<!-- 本文表示 -->
-<body>
-<!-- 表示のための値取得 -->
+
+<!-- 本文表示のための値取得 -->
 <?php
+// 各値の取得
+$familyName = $_POST["familyName"];
+$firstName = $_POST["firstName"];
+$familyHiraganaName = $_POST["familyHiraganaName"];
+$firstHiraganaName = $_POST["firstHiraganaName"];
+$phoneNumber = $_POST["phoneNumber"];
+$email = $_POST["email"];
+$consultation = $_POST["consultation"];
+$consultationContent = $_POST["consultationContent"];
 
+// メール本文の繰り返し部分を事前に変数へ設定
+$mailBodyParts = "";
+for($i=0;$i<count($consultation);$i++) { 
+  $mailBodyParts .= "・";
+  $mailBodyParts .= $consultation[$i];
+  if ($i>=count($consultation)) {
+    break;
+  }
+  $mailBodyParts .= "<br>";
+};
 
+// メール本文の文字列作成
+$mailBody = "
+以下の内容で受付いたしました。<br><br>
+お名前：{$familyName}{$firstName}<br>
+ふりがな：{$familyHiraganaName}{$firstHiraganaName}<br>
+電話番号：{$phoneNumber}<br>
+メールアドレス：{$email}<br>
+ご相談の内容：<br>{$mailBodyParts}<br>
+本文：{$consultationContent}
+";
+
+// メールタイトル指定
+$subject = "問い合わせを受付いたしました";
+// メール宛先指定
+$toMail = $email;
+// メールの送り元指定(XOAUTH認証に使うメールアドレス)
+$fromMail = "hamamura.ryohe@rexcross.com";
+// Ccで送るメール宛先指定
+$mailCc = "hamamura.ryohe@rexcross.com";
+// Bccで送るメール宛先指定
+$mailBcc = "";
 ?>
+
+
+<!-- 本文表示 -->
+<!-- ここから表示画面本文 -->
+<body class="confirm">    
+    <h2>送信完了</h2>
+    <div class="confirm-message">
+        <p>以下の内容で受付いたしました。</p>
+        <p>受付完了メールをお送りしておりますので、ご確認ください。</p>
+    </div>
+    <div class="confirmForm">
+        <div class="confirmFormBox">
+            <div class="confirmForm-item confirmForm-item1"><!-- お名前 -->
+                <p class="confirmForm-item-left confirmForm-item-left1">
+                    お名前：
+                </p> 
+                <div class="confirmForm-item-right confirmForm-item-right1">
+                    <div class="confirmForm-item-right1-1"><?php echo $familyName; ?></div>
+                    <div class="confirmForm-item-right1-2"><?php echo $firstName; ?></div>
+                </div>
+            </div>
+            <div class="confirmForm-item confirmForm-item2"><!-- ふりがな -->
+                <p class="confirmForm-item-left confirmForm-item-left2">
+                    ふりがな：
+                </p>
+                <div class="confirmForm-item-right confirmForm-item-right2">
+                    <div class="confirmForm-item-right2-1"><?php echo $familyHiraganaName; ?></div>
+                    <div class="confirmForm-item-right2-2"><?php echo $firstHiraganaName; ?></div>
+                </div> 
+            </div>
+            <div class="confirmForm-item confirmForm-item3"><!-- 電話番号 -->
+                <p class="confirmForm-item-left confirmForm-item-left3">
+                    電話番号：
+                </p> 
+                <div class="confirmForm-item-right confirmForm-item-right3"><?php echo $phoneNumber; ?></div>
+            </div>
+            <div class="confirmForm-item confirmForm-item4"><!-- メールアドレス -->
+                <p class="confirmForm-item-left confirmForm-item-left4">
+                    メールアドレス：
+                </p> 
+                <div class="confirmForm-item-right confirmForm-item-right4"><?php echo $email; ?></div>
+            </div>
+            <div class="confirmForm-item confirmForm-item5"><!-- 相談内容 -->
+                <p class="confirmForm-item-left confirmForm-item-left5">
+                    ご相談の内容：
+                </p> 
+                <div class="confirmForm-item-right confirmForm-item-right5">
+                    <?php for ($i=0;$i<count($consultation);$i++) { ?>
+                        <div class="confirmForm-item-right5List">・<?php echo $consultation[$i]; ?></div>
+                    <?php } ?>
+                </div>
+            </div>
+            <div class="confirmForm-item confirmForm-item6"><!-- 本文 -->
+                <p class="confirmForm-item-left confirmForm-item-left6">
+                    本文：
+                </p> 
+                <div class="confirmForm-item-right confirmForm-item-right6"><?php echo $consultationContent; ?></div>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
 
@@ -33,28 +129,22 @@ use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 use PHPMailer\PHPMailer\OAuth; //  ### 追加 ### 
 
-
 // Alias the League Google OAuth2 provider class
 use League\OAuth2\Client\Provider\Google;//  ### 追加 ### 
 
-
 // Composer のオートローダーの読み込み（ファイルの位置によりパスを適宜変更）
 require '../php_mailer/vendor/autoload.php';
-
 
 //mbstring の日本語設定
 mb_language("japanese");
 mb_internal_encoding("UTF-8");
 
-
 // インスタンスを生成（引数に true を指定して例外 Exception を有効に）
 $mail = new PHPMailer(true);
-
 
 //日本語用設定
 $mail->CharSet = "iso-2022-jp";
 $mail->Encoding = "7bit";
-
 
 try {
   //サーバの設定
@@ -70,11 +160,9 @@ try {
   $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS; 
   $mail->Port = 465;  //ポートの指定
  
-
-
 // ###  OAUTH2 の設定  ### 
   //Gmail メールアドレス
-  $google_email = 'hamamura.ryohe@rexcross.com';
+  $google_email = $fromMail;
   //クライアント ID
   $clientId = '583713080528-pgpnofh6331nemjjutkqddbqbiipfufg.apps.googleusercontent.com';
   //クライアントシークレット
@@ -108,31 +196,27 @@ try {
 
 //受信者設定
   //差出人アドレス（Gmail のアドレス）, 差出人名 
-  $mail->setFrom($google_email, mb_encode_mimeheader('差出人名'));  //  ### 変更 ### 
+  $mail->setFrom($fromMail, mb_encode_mimeheader('差出人名'));  //  ### 変更 ### 
   // 送信先アドレス, 受信者名（受信者名はオプション）
-  $mail->addAddress('r.hamamura1989@gmail.com', mb_encode_mimeheader("受信者名")); 
+  $mail->addAddress($toMail, mb_encode_mimeheader("受信者名")); 
   // 追加の受信者（受信者名は省略可能）
 //   $mail->addAddress('xxxxxx@xxxxxx.com'); 
   //返信用アドレス（差出人以外に必要であれば）
 //   $mail->addReplyTo('info@example.com', mb_encode_mimeheader("お問い合わせ"));  
   //Cc 受信者の指定
-  $mail->addCC('hamamura.ryohe@rexcross.com'); 
+  $mail->addCC($mailCc); 
 
- 
   //コンテンツ設定
-  $mail->isHTML(true);   // HTML形式を指定
+  $mail->isHTML(true);   // HTML形式を指定→取り消し
   //メール表題（タイトル）
-  $mail->Subject = mb_encode_mimeheader('問い合わせ完了メール'); 
+  $mail->Subject = mb_encode_mimeheader($subject); 
   //本文（HTML用）
-  $mail->Body  = mb_convert_encoding("こんにちは","JIS","UTF-8");  
+  $mail->Body  = mb_convert_encoding($mailBody,"JIS","UTF-8");  
   //テキスト表示の本文
-  $mail->AltBody = mb_convert_encoding('プレインテキストメッセージ non-HTML mail clients',"JIS","UTF-8"); 
+  $mail->AltBody = mb_convert_encoding($mailBody,"JIS","UTF-8"); 
  
-
   $mail->send();  //送信
-  echo 'Message has been sent';
-
-
+  
 //   ここまでtryの終了
 } catch (Exception $e) {
   echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
